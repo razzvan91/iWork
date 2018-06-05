b56824a0bed1622de47ca2696eb811386f7300cb
Razvan Armeanu <razvan91@gmail.com>
2018-06-05 20:14:36 +0300

    added the rest of the files

---
+++ iWork/iWork/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iWork
+//
+//  Created by Razvan Armeanu on 05/06/2018.
+//  Copyright © 2018 Razvan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
