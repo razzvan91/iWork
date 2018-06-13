14f5027c62ba45f1410b4ff79b2abd4a503de7a6
Razvan <razvan91@gmail.com>
2018-06-13 11:15:56 +0300

    added xib file and class

---
+++ Xibs/GFWorkerCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GFWorkerCellTableViewCell.swift
+//  iWork
+//
+//  Created by Razvan Armeanu on 13/06/2018.
+//  Copyright © 2018 Razvan. All rights reserved.
+//
+
+import UIKit
+
+class GFWorkerCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
